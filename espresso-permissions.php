8eec085ba983c71d8b1a5461ee32fb4ae36668a2
Darren Ethier <darren@roughsmootheng.in>
2013-07-17 08:42:57 -0400

    bump version update pue updater init to work with new pue-client in core [touch: 3180]

--- espresso-permissions.php
+++ espresso-permissions.php
@@ -3,7 +3,7 @@
 Plugin Name: Event Espresso - Permissions
 Plugin URI: http://www.eventespresso.com
 Description: Provides support for allowing members of the espreesso_event_admin role to administer events.
-Version: 1.5.4
+Version: 1.5.5.b
 Author: Event Espresso
 Author URI: http://www.eventespresso.com
 Copyright 2013  Event Espresso  (email : support@eventespresso.com)
@@ -11,7 +11,7 @@ Copyright 2013  Event Espresso  (email : support@eventespresso.com)
 
 //Define the version of the plugin
 function espresso_manager_version() {
-	return '1.5.4';
+	return '1.5.5.b';
 }
 
 //Update notifications
@@ -25,13 +25,18 @@ function ee_permissions_basic_load_pue_update() {
 		require(EVENT_ESPRESSO_PLUGINFULLPATH . 'class/pue/pue-client.php' );
 		$api_key = $org_options['site_license_key'];
 		$host_server_url = 'http://eventespresso.com';
-		$plugin_slug = 'espresso-permissions-basic';
+		$plugin_slug = array(
+			'premium' => array('p', 'espresso-permissions-basic'),
+			'prerelease' => array('b', 'espresso-permissions-basic-pr')
+			);
 		$options = array(
 			'apikey' => $api_key,
 			'lang_domain' => 'event_espresso',
 			'checkPeriod' => '24',
 			'option_key' => 'site_license_key',
-			'options_page_slug' => 'event_espresso'
+			'options_page_slug' => 'event_espresso',
+			'plugin_basename' => plugin_basename(dirname(__FILE__)),
+			'use_wp_update' => FALSE, //if TRUE then you want FREE versions of the plugin to be updated from WP
 		);
 		$check_for_updates = new PluginUpdateEngineChecker($host_server_url, $plugin_slug, $options); //initiate the class and start the plugin update engine!
 	}
